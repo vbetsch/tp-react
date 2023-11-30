7911ff433fa7a1e0528c3bd61e65159385388bf0
vbetsch <vic.betsch@gmail.com>
2023-11-30 11:55:55 +0100

    :sparkles: Get Posts

--- src/modules/miniblog/MiniBlogPage.tsx
+++ src/modules/miniblog/MiniBlogPage.tsx
@@ -1,9 +1,34 @@
 import {MainPage} from "../../components/templates/MainPage.tsx";
+import {useEffect, useState} from "react";
 
 export const MiniBlogPage = () => {
+    const [loading, setLoading] = useState(false)
+    const [repositories, setRepositories] = useState([])
+
+    const getPosts = async () => {
+        setLoading(true)
+        const data = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const json = await data.json()
+        setRepositories(json)
+    }
+
+    useEffect(() => {
+        getPosts()
+            .catch(console.error)
+            .finally(() => setLoading(false))
+    }, [])
+
+    if (loading) {
+        return (<span>Loading...</span>)
+    }
+
     return (
         <MainPage title={"MiniBlog"}>
-            <p>miniblog</p>
+            <div className="posts">
+                {repositories.map((repo: MiniBlogPost, key) => (
+                    <p key={key}>{repo.title}</p>
+                ))}
+            </div>
         </MainPage>
     )
 }
---
+++ src/modules/miniblog/MiniBlogPostType.ts
@@ -0,0 +1,6 @@
+interface MiniBlogPost {
+    userId: number
+    id: number
+    title: string
+    body: string
+}

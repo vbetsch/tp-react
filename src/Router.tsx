0c9d28bfa27f7b5633d71fdc73f42bbf842a345c
vbetsch <vic.betsch@gmail.com>
2023-11-30 11:07:46 +0100

    :truck: Rename services to modules

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,5 +1,5 @@
 import {createBrowserRouter, Navigate} from "react-router-dom";
-import {MorpionPage} from "./services/morpion/MorpionPage.tsx";
+import {MorpionPage} from "./modules/morpion/MorpionPage.tsx";
 
 export const Router = createBrowserRouter([
     {
